d61e717054686fba2f36ad1c1151f64f99d6e2ef
go852 <ccjc2030@outlook.com>
2025-01-04 16:03:37 +0800

    Create tuple.py

---
+++ tuple/tuple.py
@@ -0,0 +1,12 @@
+# 空元组
+empty_tuple = ()
+
+# 单个元素的元组（需要加逗号）
+single_element_tuple = (1,)
+print(single_element_tuple)
+# 多个元素的元组
+multi_element_tuple = (1, 2, 3, 4)
+print(multi_element_tuple)
+# 不加括号创建元组（通过逗号分隔元素）
+tuple_without_parentheses = 1, 2, 3
+print(tuple_without_parentheses)
